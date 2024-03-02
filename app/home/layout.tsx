14b45f7e26944948f664d4d55b79deb89f55e13d
wends05 <cxnner05@gmail.com>
2024-03-03 00:08:01 +0800

    update

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -11,7 +11,7 @@ function Layout({ children }: { children: React.ReactNode }) {
       {loading ? (
         <RotatingLines />
       ) : !user ? (
-        <div cl              assName="flex flex-col items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center justify-center min-h-screen">
           <h1>Not logged in</h1>
         </div>
       ) : (
